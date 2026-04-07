992f7a0762531aff371f856119492a61b053867c
remohelg <remo.helg@gmx.ch>
2026-04-07 18:33:28 +0200

    statuc to dynamic

--- src/lib/db.server.js
+++ src/lib/db.server.js
@@ -1,7 +1,7 @@
 import { MongoClient, ObjectId } from "mongodb"; // See https://www.mongodb.com/docs/drivers/node/current/quick-start/
-import { DB_URI } from "$env/static/private";
+import { env } from "$env/dynamic/private";
 
-const client = new MongoClient(DB_URI);
+const client = new MongoClient(env.DB_URI);
 
 await client.connect();
 const db = client.db("ScreenStackDB"); // select database
